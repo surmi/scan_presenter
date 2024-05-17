945ff7828ef80d5a8a1909f1ab8ccd63d4e31157
surmi <surmi@users.noreply.github.com>
2024-05-17 21:01:01 +0200

    basic next.js app

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+import LikeButton from "./like-button";
+function Header({title}){
+    return <h1>{title ? title : 'Default Title'}</h1>;
+}
+
+export default function HomePage(){
+    const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+    return <div>
+        <Header title="React" />
+        <ul>
+            {names.map((name) => (
+                <li key={name}>{name}</li>
+            ))}
+        </ul>
+        <LikeButton/>
+    </div>;
+}
